6b0dbbdcf94c78e43cc0452fa67d3b7bf50d4efd
Emelie Olsson <emelieannaolsson@gmail.com>
2022-09-15 20:44:20 +0200

    Failing test

--- Test/server.test.js
+++ Test/server.test.js
@@ -4,7 +4,4 @@ import Server from '../server';
 
 if('renders without crashing', () => {
     expect(1).toEqual(2)
-    const div = document.createElement('div');
-    ReactDOM.render(<Server />, div);
-    ReactDOM.unmountComponentAtNode(div)
 });
