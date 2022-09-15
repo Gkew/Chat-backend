03362502c2783a1e59d833c74349705953d2497f
Emelie Olsson <emelieannaolsson@gmail.com>
2022-09-15 20:39:20 +0200

    testing

--- Test/server.test.js
+++ Test/server.test.js
@@ -1,7 +1,10 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Server from '../server';
 
 if('renders without crashing', () => {
     expect(1).toEqual(2)
-
+    const div = document.createElement('div');
+    ReactDOM.render(<Server />, div);
+    ReactDOM.unmountComponentAtNode(div)
 });
