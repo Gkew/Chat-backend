cf487873b0e518cf615409d93240405081ece8c1
Emelie Olsson <emelieannaolsson@gmail.com>
2022-09-19 12:07:19 +0200

    added a route for test

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,6 @@ const router = require("express").Router();
 
 router.post("/adduser", addUser)
 router.get("/allusers/:id" , allUsers);
+router.get("/allusers" , allUsers);
 
 module.exports = router;
