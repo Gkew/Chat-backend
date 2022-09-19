43cd9ca927e28ed31c3a42e2d098c07bc1539a88
Emelie Olsson <emelieannaolsson@gmail.com>
2022-09-19 11:33:07 +0200

    Adding some swagger

--- Test/component.js
+++ Test/component.js
@@ -1,4 +1,7 @@
- const { expect } = require('chai');
+/**
+ * @group component
+ */
+const { expect } = require('chai');
  const chai = require('chai')
  const chaiHttp = require('chai-http');
  chai.use(chaiHttp);
--- Test/husky.test.js
+++ Test/husky.test.js
@@ -1,5 +1,7 @@
-
-test("hello wrold", async() => {
+/**
+ * @group dummy test
+ */
+test("Husky dummy test", async() => {
     console.log('test')
     expect(2).toEqual(2)
     expect(1).toEqual(1)
