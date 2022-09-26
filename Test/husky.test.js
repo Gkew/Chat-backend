8e110859b97fd42d2bdd0335b4a1c8dafbfab993
AlexD1337 <aalexdanil@gmail.com>
2022-09-26 19:55:40 +0200

    supertest working

--- Test/husky.test.js
+++ Test/husky.test.js
@@ -2,7 +2,6 @@
  * @group dummy test
  */
 test("Husky dummy test", async() => {
-    console.log('test')
     expect(2).toEqual(2)
     expect(1).toEqual(1)
 });
--- controller/messageController.js
+++ controller/messageController.js
@@ -1,14 +1,22 @@
 const messageModel = require("../Models/messageSchema")
+var Stopwatch = require('timer-stopwatch');
 
-module.exports.addMessage = async (req, res, next) => {
+
+module.exports.addMessage = async (req, res) => {
 
     try{
+        var stopwatch = new Stopwatch();
         const {from,to ,message} = req.body;
-        const data = await messageModel.create({
+
+
+        const mezzage = {
             message:{text:message},
             users: [from, to],
             sender: from,
-        });
+        }
+
+        const data = messageModel.create(mezzage);
+
         if(data) return res.json({msg:"Message sent"});
         return res.json({ msg: "Something went wrong"})
     }
@@ -40,3 +48,27 @@ module.exports.getAllMessages = async (req, res, next) => {
 //
 // [{fromSelf:true, message:"this is a message"}, {fromSelf:true, message:"this is a message"}, {fromSelf:true, message:"this is a message"}]
 //
+
+// Post a new user
+//  const newUser = {
+//   userId : "aJQwXgDy5NMkrwgAzsSoMYA6adaw2",
+//   displayName : "test_name",
+//   photoUrl : "https://lh3.googleusercontent.com/a/AItbvmmJLXCNJcMRMo3C_TsrVL-7sbEUeeHSyJHUprvz=s96-c"
+// }
+// describe("POST a new user", () => {
+//   it("should add new user", (done) => {
+//       chai.request(BACKEND_HOST)
+//       .post('/user/adduser')
+//       .send(newUser)
+//       .end((err, res) => {
+//           expect(res.statusCode).to.equal(200);
+//           expect(res.body).to.not.be.null;
+//           const lastItem = JSON.parse(res.text)
+//           expect(lastItem.user.userId).to.equal(newUser.userId);
+//           expect(lastItem.user.displayName).to.equal(newUser.displayName);
+//           expect(lastItem.user.photoUrl).to.equal(newUser.photoUrl);
+//         done()
+        
+//       })
+// })
+// })
